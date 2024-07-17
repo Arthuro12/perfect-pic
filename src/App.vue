a9ba36c181a3bc15be4d6e97ee21ee3362cd67e3
Arthuro12 <arthurfoadjo@yahoo.com>
2024-07-17 22:04:16 +0200

    Add footer page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import PpHeader from "./components/layouts/PpHeader.vue";
+import PpFooter from "./components/layouts/PpFooter.vue";
 </script>
 
 <template>
   <PpHeader />
   <main></main>
+  <PpFooter />
 </template>
 
 <style scoped></style>
---
+++ src/components/layouts/PpFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="page-footer">
+    <div class="page-footer__inner">
+      <div class="page-footer__row">
+        <header>
+          <h3>Perfect-Pic</h3>
+          <ul>
+            <li><a href="#">About Us</a></li>
+          </ul>
+          <h3>Support</h3>
+          <ul>
+            <li><a href="#">FAQs</a></li>
+          </ul>
+        </header>
+      </div>
+      <div class="page-footer__row">
+        <p><span>Copyright &copy; Perfect-Pic 2024</span></p>
+      </div>
+    </div>
+  </footer>
+</template>
